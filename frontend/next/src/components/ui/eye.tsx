a505bd60fa9d0818aa9bd01189f77db3d2940796
hargunmujral <hargunmujral@gmail.com>
2024-06-23 11:03:06 -0700

    added pulsating eye

---
+++ frontend/next/src/components/ui/eye.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Eye.css';
+
+const Eye = () => {
+  return (
+    <div className="eye-container">
+      <div className="eye">
+        <div className="pupil"></div>
+      </div>
+    </div> 
+  );
+};
+
+export default Eye;
