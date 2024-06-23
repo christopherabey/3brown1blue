3f9ecd233bf770688f41b4da13d5e6302d623d83
Krish Shah <krish.life21@gmail.com>
2024-06-22 18:00:01 -0700

    add openai

--- backend/experiments.py
+++ backend/experiments.py
@@ -1,2 +1,8 @@
 from scene_generator import SceneGenerator
 
+test_transcriptions = [
+    "The quick brown fox jumps over the lazy dog.",
+]
+
+sg = SceneGenerator(test_transcriptions)
+sg.generate_all_scenes()
